14718acab5f26a63af43fa337197facd58f50b84
Alexandre Nascimento <Alexandre.Nascimento.T0118447@edu.atec.pt>
2021-07-24 16:03:18 +0100

    Alteraçoes a nivel geral

---
+++ Oasis.Web/ViewModels/PerfilUtilizadorViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Oasis.Dominio.Entidades;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oasis.Web.ViewModels {
+    public class PerfilUtilizadorViewModel {
+
+
+        public IEnumerable<ApplicationUser> UtilizadoresEscola { get; set; }
+
+        public int TemasId { get; set; }
+        public IEnumerable<SelectListItem> TemasDropDown { get; set; }
+    }
+}
