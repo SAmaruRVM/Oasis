77dd5768741a9a1aacfcee3a60bd707f339794fc
Alexandre Nascimento <Alexandre.Nascimento.T0118447@edu.atec.pt>
2021-07-27 16:24:33 +0100

    Ajustes de layout

---
+++ Oasis.Web/Areas/Alunos/Controllers/BaseAlunoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oasis.Web.Areas.Aluno.Controllers
+{
+
+    [Area("Alunos")]
+    [Authorize(Roles = "Aluno")]
+    public class BaseAlunoController : Controller
+    {
+    }
+}
